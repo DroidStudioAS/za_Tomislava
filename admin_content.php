d53a8ddb9649435e167a463bd6000ab0e74ec2ef
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-27 13:55:17 +0100

    dodao prikazivanje forme za edit

--- admin_content.php
+++ admin_content.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Title Here</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="public/styles.css"/>
 </head>
 <body>
     <!-- Your HTML content here -->
@@ -12,20 +13,40 @@
     <div id="user_container">
 
     </div>
+    <div style="display: none;" id="edit_user_container" class="edit_user_container">
+        <form class="edit_form">
+            <h1 id="user_h"></h1>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="submit"/>
+        </form>
+    </div>
     <script>
         let userContainer = $("#user_container");
+        let editContainer = $("#edit_user_container")
        
+        /*****Helper functions******/
         function handleEditUser(user){
             console.log(user)
+            editContainer.css('display','flex');
+
         }
+        //render all users fetched by getAllUsers();
         function displayUsers(userData) {
             let users = JSON.parse(userData);
             users.forEach(function(user) {
             let userElement = document.createElement('div');
             userElement.className = 'user';
+            if(user.isAdmin===0){
+                console.log('not')
+            }
             userElement.innerHTML = '<p>Username: ' + user.username + '</p>' +
                                 '<p>Email: ' + user.email + '</p>' +
-                                '<p>Age: ' + user.age + '</p>';
+                                '<p>Age: ' + user.age + '</p>' +
+                                '<p>Admin: ' + (user.isAdmin==1 ? 'Yes' : 'No') + '</p>'; // Display 'Yes' if isAdmin is 1, 'No' otherwise
+;
 
             // Create edit button
             let editButton = document.createElement('button');
@@ -63,6 +84,7 @@
               });
         }
 
+    
     $('document').ready(function(){
         getAllUsers();
     })

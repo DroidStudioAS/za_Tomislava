47befa6a54b42c7c83a0431bdf315315c4d65357
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-27 11:12:54 +0100

    intitial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Title Here</title>
+    <!--Jquery import-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXhBvqhYGzXXuRauk=" crossorigin="anonymous"/>
+</head>
+<body>
+    <div id="registration-container">
+    <form action=""></form>
+    </div>
+    <div id="login_container">
+    <form action=""></form>
+    </div>
+</body>
+</html>

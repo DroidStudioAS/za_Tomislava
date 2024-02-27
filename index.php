b7a13a14f81b87cdd8526571cbcd66ff0de943f1
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-27 11:34:35 +0100

    added toggling between reg and login forms

--- index.php
+++ index.php
@@ -5,14 +5,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Title Here</title>
     <!--Jquery import-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXhBvqhYGzXXuRauk=" crossorigin="anonymous"/>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <div id="registration-container">
-    <form action=""></form>
+    <form action="">
+        <input id="register_username" type="text" placeholder="username"/>
+        <input id="register_password" type="password" placeholder="Password"/>
+        <input id="register_email" type="email" placeholder="Email"/>
+        <input id="register_age" type="number" placeholder="Age"/>
+        <input id="register_trigger" type="submit" value="Submit">
+    </form>
     </div>
-    <div id="login_container">
-    <form action=""></form>
+
+    <div style="display:none;" id="login_container">
+    <form action="">
+        <input id="login_username" type="text" placeholder="username"/>
+        <input id="login_password" type="password" placeholder="Password"/>
+        <input id="login_trigger" type="submit" value="Submit">
+    </form>
+
     </div>
+
+    <div id="toggle_login_reg">Already Have An Account? Click Here To Login!</div>
+    <script>
+        let onRegister = true;
+
+        //Ui references
+        let toggleButton = $('#toggle_login_reg');
+        let registrationContainer = $("#registration-container");
+        let loginContainer = $("#login_container");
+        // Registration Form Inputs
+        let registerUsername = $("#register_username");
+        let registerPassword = $("#register_password");
+        let registerEmail = $("#register_email");
+        let registerAge = $("#register_age");
+        let registerTrigger = $("#register_trigger");
+        
+        // Login Form Inputs
+        let loginUsername = $("#login_username");
+        let loginPassword = $("#login_password");
+        let loginTrigger = $("#login_trigger");
+
+        /*****OnClickListeners*****/
+        //toggle button (switches between login and reg form)
+        toggleButton.off('click').on('click',function(){
+            onRegister=!onRegister;
+            registrationContainer.toggle();
+            loginContainer.toggle();
+
+            console.log(onRegister);
+
+            if(onRegister===true){
+                toggleButton.text('Already Have An Account? Click Here To Login!')
+            }else{
+                toggleButton.text('Dont Have An Account? Click Here To Register!')
+            }        
+        })
+
+    </script>
 </body>
 </html>
